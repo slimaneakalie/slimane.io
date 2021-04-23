8742adda9682eeb3bb0885f1fd4b375ace0504c3
Slimane Akalie <slimaneakaliasp@gmail.com>
2021-04-23 17:33:24 +0000

    chore: initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,10 @@
 
 import { Provider } from 'react-redux'
-import { useStore } from '../store'
+// import { useStore } from '../store'
 import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps } : AppProps) {
-    const store = useStore(pageProps.initialReduxState)
-
     return (
-        <Provider store={store}>
             <Component {...pageProps} />
-        </Provider>
     )
 }
