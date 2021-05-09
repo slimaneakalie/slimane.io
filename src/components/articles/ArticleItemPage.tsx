d9a4145168318c8bdfe0e36a78b993aa2b9c40c5
Slimane Akalie <slimaneakaliasp@gmail.com>
2021-05-09 17:58:05 +0000

    fix: add the article matadata

--- src/components/articles/ArticleItemPage.tsx
+++ src/components/articles/ArticleItemPage.tsx
@@ -3,18 +3,26 @@ import PortableTextPresenterProps from "../../lib/sanity.io/PortableTextPresente
 import { useArticleItemPageStyles } from "../../styles/articles/articleItemPage.styles";
 import ImageTitle from "../shared/ImageTitle";
 import ArticleMetadata from "./ArticleMetadata";
+import { Head } from "next/document";
 
 export default function ArticleItemPage({
   article,
 }: ArticleItemPageProps): JSX.Element {
   const classes = useArticleItemPageStyles();
   return (
-    <article className={classes.root}>
-      <ImageTitle mainImageUrl={article.thumbnailURL} title={article.title} />
-      <div className={classes.bodyContainer}>
-        <ArticleMetadata article={article} />
-        {article.body && <PortableTextPresenterProps body={article.body} />}
-      </div>
-    </article>
+    <>
+      <Head>
+        <meta property="og:title" content={article.title} />
+        <meta property="og:image" content={article.thumbnailURL} />
+        <meta property="og:description" content={article.shortDescription} />
+      </Head>
+      <article className={classes.root}>
+        <ImageTitle mainImageUrl={article.thumbnailURL} title={article.title} />
+        <div className={classes.bodyContainer}>
+          <ArticleMetadata article={article} />
+          {article.body && <PortableTextPresenterProps body={article.body} />}
+        </div>
+      </article>
+    </>
   );
 }
