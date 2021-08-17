cf2942125f698eab3c1bf461562c74ef9611f3d9
Slimane Akalië <slimaneakalia@gmail.com>
2021-08-18 00:10:27 +0100

    Remove books link from the header

--- src/containers/shared/header.container.tsx
+++ src/containers/shared/header.container.tsx
@@ -12,10 +12,10 @@ const navbarProps: HeaderProps = {
       label: "Articles",
       link: "/articles",
     },
-    {
-      label: "Books",
-      link: "/books",
-    },
+//     {
+//       label: "Books",
+//       link: "/books",
+//     },
     {
       label: "Podcast",
       link: "/podcast",
