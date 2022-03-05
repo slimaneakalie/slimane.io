3243db8d48f4afdd462e78e50a617713e2e09f09
Slimane Akalië <slimaneakalia@gmail.com>
2022-03-05 01:17:28 +0100

    fix: build issues

--- src/types/shared/store.types.ts
+++ src/types/shared/store.types.ts
@@ -1,12 +1,12 @@
 import { HomeState } from "../home/store.home.types";
 import { ArticlesState } from "../articles/store.articles.types";
 import { PodcastState } from "../podcast/store.podcast.types";
-import { AudioPlatforms } from "./whereToListen.types";
+import { AudioPlatform } from "./whereToListen.types";
 import { BooksPageState } from "../books/store.books.types";
 
 export interface SharedState {
   darkModeActivated: boolean;
-  audioPlatforms: AudioPlatforms[];
+  audioPlatforms: AudioPlatform[];
 }
 
 export interface State {
