fc7081529d9da26089b758ba3885f174f0ee7ebd
Slimane Akalië <30215409+slimaneakalie@users.noreply.github.com>
2022-09-17 16:39:09 +0100

    Update initializeState.ts

--- src/store/home/initializeState.ts
+++ src/store/home/initializeState.ts
@@ -19,6 +19,10 @@ export async function getHomeInitialState(): Promise<HomeState> {
 
 const latestVideos = [
   {
+    id: "-ELmGkZQNfc",
+    title: "مهدي شراشر: أساسيات البرمجة، Golang، Rust، العمل في Meta، ليونيل ميسي، الزواج، الموسيقى، السعادة",
+  },
+  {
     id: "R0G_xRYWlEA",
     title:
       "يوسف التبالي: الحلم المغربي، البودكاست، الزواج، العلاقات، المرأة في الإسلام، الطب، الشهرة، السياسة",
@@ -39,10 +43,6 @@ const latestVideos = [
   {
     id: "rH0NVA2my4E",
     title: "ياسر طاهري: موسيقى الراب، Python, تعلم البرمجة، Open Source, الفلسفة الإغريقية، Secure code, الزواج",
-  },
-  {
-    id: "O2JhdJHNu28",
-    title: "غيثة منير: العيش في كاليفورنيا، الشركات الناشئة، الفروسية، اللغات، الأمن المعلوماتي، التوازن، الشغف",
   },
 ];
 
