0da0d94aae578778ec3eccf1d2f8f0ece1fa7400
Slimane Akalië <30215409+slimaneakalie@users.noreply.github.com>
2022-09-18 17:30:42 +0100

    test twitter card

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -31,10 +31,14 @@ export default function ArticleItem(props: Article): JSX.Element {
         <meta property="og:title" content={props.title} />
         <meta property="og:image" content={props.thumbnailURL} />
         <meta property="og:description" content={props.shortDescription} />
+        <meta name="description" content={props.shortDescription} />
+
+        <meta name="twitter:card" content="summary_large_image"/>
         <meta property='twitter:title' content={props.title}/>
         <meta property='twitter:image' content={props.thumbnailURL}/>
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="description" content={props.shortDescription} />
+        <meta name="twitter:creator" content="@cd_slimane"></meta>
+        <meta name="twitter:site" content="@cd_slimane"></meta>
+        <meta name="twitter:description" content={props.shortDescription}></meta>
       </Head>
       <ArticleItemPageContainer article={props} />
     </>
